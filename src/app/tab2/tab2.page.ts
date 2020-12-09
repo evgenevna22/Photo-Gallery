1d1772eec4dc703e30e808ad23aff5b4aaf9e14c
evgenevna22 <tanya.evgenevna@gmail.com>
2020-12-09 22:14:24 +0300

    added removing photo logic

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActionSheetController } from '@ionic/angular';
+import { Photo } from '../models/photo.model';
 import { PhotoService } from '../services/photo.service';
 
 @Component({
@@ -6,18 +8,63 @@ import { PhotoService } from '../services/photo.service';
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page implements OnInit {
+export class Tab2Page implements OnInit, OnDestroy {
 
   constructor(
-    public photoService: PhotoService
+    public photoService: PhotoService,
+    private actionSheetController: ActionSheetController
   ) {}
 
   async ngOnInit(): Promise<void> {
+    console.log('NgOnInit');
     await this.photoService.loadSaved();
   }
 
+  ionViewWillEnter() {
+    console.log('ionViewWillEnte');
+  }
+
+  ionViewDidEnter() {
+    console.log('ionViewDidEnter');
+  }
+
+  ionViewWillLeave() {
+    console.log('ionViewWillLeave');
+  }
+
+  ionViewDidLeave() {
+    console.log('ionViewDidLeave');
+  }
+
+  ngOnDestroy() {
+    console.log('OnDestroy');
+  }
+
+
   public addPhotoToGallery(): void {
     this.photoService.addNewToGallery();
+  }
+
+  public async showActionSheet(photo: Photo, index: number): Promise<void> {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Photos',
+      buttons: [{
+        text: 'Delete',
+        role: 'destructive',
+        icon: 'trash',
+        handler: () => {
+          this.photoService.deletePicture(photo, index);
+        }
+      }, {
+        text: 'Cancel',
+        icon: 'close',
+        role: 'cancel',
+        handler: () => {
+          // Nothing to do, action sheet is automatically closed
+          }
+      }]
+    });
+    await actionSheet.present();
   }
 
 }
